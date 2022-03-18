6491f60f80d556db500611c6f3b766a9b4cabc2f
Santi D <santid13@hotmail.com>
2022-03-18 21:26:23 +0000

    working on exercises in easy4

---
+++ easy4/multiples3_5_further_exploration.rb
@@ -0,0 +1,35 @@
+def modulo_multiply (num, divisor)
+  num % divisor == 0
+end 
+
+def multisum(max_num)
+  total_array = []
+  1.upto(max_num) do |n| 
+    if modulo_multiply(n, 3) || modulo_multiply(n, 5)
+      total_array << n
+    end 
+  end 
+  total_array.inject(:+) 
+end 
+
+p multisum(3) 
+p multisum(5) 
+p multisum(10) 
+p multisum(1000)
+  
+    
+
+
+#5 x 4 = 20
+#5  x       num
+#input: a number
+#output a number - sum of all multiples of 3 n 5 to the number provided
+#multiples: 3, 6, 9, 12, 5, 10, 15, 20 etc. 
+#obtain all multiples of 3 up to number provided
+  # counter to limit up to when to divide
+  #obtain multiples: if statement of n % 3 == 0 
+  # iterate until nreaches counter
+#repeat for 5.
+#push all numbers to one array
+# use sum method to add all numbers
+#return number
---
+++ easy4/running_totals.rb
@@ -0,0 +1,16 @@
+def running_total(array) #[ 2, 5, 13 ] 
+  index = 0
+  
+  array.map do |number|
+    array << number + number[index] 
+  end
+
+end 
+  
+p running_total([2, 5, 13])
+
+#input: array of num
+#output: new array of num
+#add arr[0] to arr[1], the sum will equal new_arry[1] and so on
+#will have to work with index numbers
+#take first and second = first new position
